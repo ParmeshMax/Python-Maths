5216d280996327ca5a96ad1d09f5d615aad6ca8e
Parmesh <parmeshadhikari29@gmail.com>
2026-03-23 12:54:09 +0545

    Volume of Cuboid and Cylinder!!
    Cuboid volume = product of its three dimensions.
    
    Cylinder volume = area of base circle × height.

---
+++ VC.py
@@ -0,0 +1,4 @@
+r=float(input("Enter the radius of the circle: "))
+pie=3.14
+area=pie*r**2
+print("The area of the circle is: ", area)
---
+++ cuboid.py
@@ -0,0 +1,5 @@
+l = int(input("Enter the Length: "))
+b = int(input("Enter the Breadth: "))
+h = int(input("Enter the Height: "))
+VOC=l*b*h
+print("Volume of the cuboid:", voc)
