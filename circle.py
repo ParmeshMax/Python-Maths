aa1ae938cef7cbaa08e62154246184e6e98e6714
NPCODER <parmeshadhikari29@gmail.com>
2026-04-03 21:24:05 +0545

    Add function to calculate area of a circle
    Refactored circle area calculation into a function.

--- circle.py
+++ circle.py
@@ -2,3 +2,12 @@ R=float(input("Enter the radius of the circle: "))
 pi=3.14
 area=pi*R**2
 print("The area of the circle is: ", area)
+
+#functions
+def area():
+    R = float(input("Enter the radius of the circle: "))
+    pi = 3.14
+    a=pi * R ** 2
+    return f"The area of the circle: {a}"
+print(area())
+  
