6c24e19257888706a64bdc38ec0c8c71aede61d5
NPCODER <parmeshadhikari29@gmail.com>
2026-04-03 21:43:47 +0545

    Update Fourwalll.py

--- Fourwalll.py
+++ Fourwalll.py
@@ -3,3 +3,12 @@ b=float(input("Enter the breadth of the room:"))
 h=float(input("Enter the height of the room:"))
 AOW=2*h*(l+b)
 print("Area of the walls is:",AOW)
+
+#function
+def AOW():
+  l=float(input("Enter the length of the room:"))
+  b=float(input("Enter the breadth of the room:"))
+  h=float(input("Enter the height of the room:"))
+  AOW=2*h*(l+b)
+  return f"Area of the walls:{AOW}"
+print(AOW())
