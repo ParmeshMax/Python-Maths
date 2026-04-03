ac266202ad93db26d9ed28118e05a1c6f079283f
NPCODER <parmeshadhikari29@gmail.com>
2026-04-03 21:39:17 +0545

    Update cuboid.py

--- cuboid.py
+++ cuboid.py
@@ -3,3 +3,12 @@ b = int(input("Enter the Breadth: "))
 h = int(input("Enter the Height: "))
 VOC=l*b*h
 print("Volume of the cuboid:", voc)
+
+#functions
+def VOC():
+  l = int(input("Enter the Length: "))
+  b = int(input("Enter the Breadth: "))
+  h = int(input("Enter the Height: "))
+  VOC=l*b*h
+  return f"Volume of the cuboid:{VOC}"
+print(VOC())
