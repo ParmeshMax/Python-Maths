3f91e219c94bfb5284e1818008f472629c014156
NPCODER <parmeshadhikari29@gmail.com>
2026-04-03 12:45:44 +0545

    Add function to calculate area of a circle
    Refactored area calculation into a function.

--- VC.py
+++ VC.py
@@ -2,3 +2,12 @@ r=float(input("Enter the radius of the circle: "))
 pie=3.14
 area=pie*r**2
 print("The area of the circle is: ", area)
+
+# function
+def area():
+    r=float(input("Enter the radius of the circle:"))
+    pie=3.14
+    area=pie*r**2
+    return f"The area of the circle:{area}"
+print(area())
+  
