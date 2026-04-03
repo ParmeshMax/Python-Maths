a1e6e2b479ba65cf917293ebc7e325d056e933de
NPCODER <parmeshadhikari29@gmail.com>
2026-04-03 12:39:30 +0545

    Add convert function for temperature conversion
    Refactor temperature conversion to use a function.

--- cvrt.py
+++ cvrt.py
@@ -1,3 +1,12 @@
 Celcius=float(input ("Enyter the temperature in Celcius: "))
 Fahenheit=(Celcius*9/5)+32
 print ("The temperature in Fahenheit is: ", Fahenheit)
+
+#Functions
+def convert():
+  Fahenheit=(Celcius*9/5)+32
+  return f"The Fahenheit value:{Fahenheit}"
+Celcius=float(input ("Enter the temperature in Celcius: "))
+print(convert())
+
+  
